bef952ba0f0f97d946a20af6e7f7189b9cb5180c
praveen dodla <dodlapraveen09@gmail.com>
2022-08-24 17:18:07 +0530

    Given mood return Happy or Sad Mood

---
+++ MoodAnalyser_MS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MoodAnalyser_MS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***MOOD_ANALYSER***");
+        }
+        public string AnalyseMood(string mood)
+        {
+            if (mood.Equals("Happy"))
+            {
+                return mood + " mood";
+            }
+            else if (mood.Equals("Sad"))
+            {
+                return mood + " mood";
+            }
+            else if (mood.Equals("I am in Sad Mood"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
